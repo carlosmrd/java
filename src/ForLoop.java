f9e7b44754d10d0775ce57348ef352acb66d0151
Carlos <carlosmrd2001@gmail.com>
2025-04-03 00:45:39 -0300

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        //for loop =  executa um código um certo número de vezes
+        //inicialização(loop control variable); condição; atualização
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
